ebd83680d7877ecce53cbc837ecc6983ddf0a5e0
Arkan <arkan501@proton.me>
2024-02-09 11:13:44 -0700

    feat: disabled Neotree, added in some remappings
    I wasn't really using neotree, but I didn't want to delete it just in
    case I want to use it later down the line. Added some much QOL
    remappings. Did some remappings with harpoon, and reformmated the code
    of the init.lua file. I'll be looking to disable text wrap in the next
    commit.

--- lua/arkan/remap.lua
+++ lua/arkan/remap.lua
@@ -1,3 +1,10 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>nt", ":Neotree<CR>")
+
+-- move selected lines in visual mode : ThePrimeagen
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") --this moves up
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") --this moves down
+
+-- appends the line below to the current line, without moving the cursor : ThePrimeagen
+vim.keymap.set("n", "J", "mzJ'z")
