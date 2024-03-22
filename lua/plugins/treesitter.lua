35eec5f3468e08e1c6da236d9517031aeef87962
Arkan <arkan501@proton.me>
2024-03-21 20:36:54 -0700

    chore: removed language support for python, kdl and Typescript
    I wasn't really using them.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         local config = require("nvim-treesitter.configs")
         config.setup({
-            ensure_installed = { "c_sharp", "kdl", "lua", "rust", "java", "typescript" },
+            ensure_installed = { "c_sharp", "lua", "rust", "java" },
             highlight = { enable = true },
             indent = { enable = true }
         })
