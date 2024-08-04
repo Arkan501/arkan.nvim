7ce7ac32083d593776e5daa5d4bd3d58efb31fb5
Arkan <arkan501@proton.me>
2024-08-04 10:24:48 -0700

    feat: dap
    created a lua file with dap configuration. Updated trouble.lua in
    accordance wit trouble v3. Added in more lsp support for go, now there
    is gofmt, goimports, and delve support included in none-ls.lua. Will
    probably remove java related things in the future.

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,62 @@
+return {
+    -- main dap thingy
+    {
+        "mfussenegger/nvim-dap",
+        config = function()
+            local dap = require("dap")
+
+            vim.keymap.set("n", "<F2>", dap.step_into, { noremap = true })
+            vim.keymap.set("n", "<F3>", dap.step_over, { noremap = true })
+            vim.keymap.set("n", "<F5>", dap.continue, { noremap = true })
+            vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { noremap = true })
+        end,
+    },
+    -- dap-ui
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+            "nvim-neotest/nvim-nio",
+        },
+        config = function()
+            require("dapui").setup()
+
+            local dap, dapui = require("dap"), require("dapui")
+            dap.listeners.before.attach.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.launch.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated.dapui_config = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited.dapui_config = function()
+                dapui.close()
+            end
+        end,
+    },
+    -- virtual text
+    {
+        "theHamsta/nvim-dap-virtual-text",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+        },
+        config = function()
+            require("nvim-dap-virtual-text").setup({
+                commented = true,
+                virt_text_pos = "eol",
+            })
+        end,
+    },
+    -- dap for go
+    {
+        "leoluz/nvim-dap-go",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+        },
+        config = function()
+            require("dap-go").setup()
+        end,
+    },
+}
--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,15 +1,34 @@
 return {
-	"folke/trouble.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		vim.keymap.set("n", "<leader>tt", function()
-			require("trouble").toggle()
-		end)
-		vim.keymap.set("n", "[t", function()
-			require("trouble").next({ skip_groups = true, jump = true })
-		end)
-		vim.keymap.set("n", "]t", function()
-			require("trouble").previous({ skip_groups = true, jump = true })
-		end)
-	end,
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    cmd = { "Trouble" },
+    opts = {
+        modes = {
+            lsp = {
+                win = { position = "bottom" },
+            },
+        },
+    },
+    keys = {
+        {
+            "<leader>tt",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "Diagnostics (Trouble)",
+        },
+        {
+            "[t",
+            "<cmd>Trouble next<cr>",
+            desc = "Next (Trouble)",
+        },
+        {
+            "]t",
+            "<cmd>Trouble previous<cr>",
+            desc = "Previous (Trouble)",
+        },
+        {
+            "<leader>tq",
+            "<cmd>Trouble quickfix<cr>",
+            desc = "Quickfix (Trouble)",
+        },
+    },
 }
