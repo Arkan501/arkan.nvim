7ceea7f74ccf61b4ef99d4fe29e458397c968bdc
Arkan <arkan501@proton.me>
2024-09-17 07:49:55 -0700

    fix: delve not loading
    I took out delve from the ensure install list for some reason under the
    mason-lspconfig plugin section. Not sure why I did that, but it was when
    I was installing python stuff, so I may have thought I wasn't supposed.
    Turns out I do in fact need delve there.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,6 +16,7 @@ return {
                 ensure_installed = {
                     -- "csharp_ls",
                     "gopls",
+                    "delve",            -- delve needs to be here
                     -- "jdtls",
                     "lua_ls",
                     "pylsp",
